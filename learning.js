5c1b1e4cb5d5234771e494631722d81309640277
Gbenga Lasisi <lasisig5@gmail.com>
2025-08-22 14:30:19 +0100

    Student Grade

--- learning.js
+++ learning.js
@@ -2,3 +2,14 @@ function addNumbers(a, b) {
     return a + b;
 }
 addNumbers(5, 10);
+
+class Score {
+    #parentsname
+    constructor(name, totalscore, grade, parentsname){
+        this.name = name
+        this.totalscore = totalscore
+        this.grade = grade
+        this.#parentsname = parentsname
+    }
+}
+
