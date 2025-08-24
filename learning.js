9ce60744fade255bd74fdf4dd0891667fb1a4e68
Gbenga Lasisi <lasisig5@gmail.com>
2025-08-25 00:23:22 +0100

    updating class score

--- learning.js
+++ learning.js
@@ -6,10 +6,25 @@ addNumbers(5, 10);
 class Score {
     #parentsname
     constructor(name, totalscore, grade, parentsname){
-        this.name = name
-        this.totalscore = totalscore
-        this.grade = grade
-        this.#parentsname = parentsname
+        this.name = name;
+        this.totalscore = totalscore;
+        this.grade = grade;
+        this.#parentsname = parentsname;
+    }
+    youPass(){
+        if(this.totalscore >= 50){
+            console.log("you passed the course")
+
+        }if (this.grade >= 40) {
+            console.log("you passed the course")
+        }else{
+            console.log("failed")
+        }
+        
     }
 }
+
+studentScore = new Score(`Adegboyega`, 70, `A`, `Ayodele` );
+studentScore.youPass();
+
 
